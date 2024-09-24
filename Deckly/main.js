efe250474d7b3a6ffe3f21b1b8afce4aab72f7b7
thickCode <118022331+fr3s4avakad00@users.noreply.github.com>
2024-09-24 14:17:03 -0400

    Add files via upload

---
+++ Deckly/main.js
@@ -0,0 +1,17 @@
+
+function createQuiz() {
+    window.location.href = 'createQuiz.html';
+    
+  }
+  
+  function createCueCard() {
+    window.location.href = 'createCueCard.html';
+  }
+  
+  function readNotes() {
+    window.location.href = 'readNote.html';
+  }
+
+  function exit() {
+    window.location.href = 'helo.html';
+  }
