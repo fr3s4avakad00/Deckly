66751aae7559ce4a53554a26f936b3f0800b4605
PricklyProgrammer <mazen.elazouni@gmail.com>
2024-09-28 17:11:46 -0400

    Sep 28 5:11

--- Deckly/account.js
+++ Deckly/account.js
@@ -1,13 +1,23 @@
-//this is the user's account
 import cueCard from './cueCard.js';
+//this is the user's account
 
-myCueCards=[];
+var myCueCards=[];
+
+const cueCardDivs=document.getElementById("cardDivs");
 
 document.addEventListener('DOMContentLoaded', function (){
     const createCueCardButton=document.getElementById("createCueCardButton");
 
     createCueCardButton.addEventListener("click", function(){
         const newCueCard=new cueCard("", "", false);
+        newCueCard.id=myCueCards.length;
         myCueCards.push(newCueCard);
+        var cueCardHomeText=document.createElement("p");
+        var button = document.createElement("button");
+        button.textContent = "Open Cue Card";
+        button.addEventListener('click', function() {
+            myCueCards[newCueCard.id].show();
+        });
+        cueCardDivs.appendChild(cueCardHomeText);
     });
 });
--- Deckly/cueCard.js
+++ Deckly/cueCard.js
@@ -1,18 +1,26 @@
 class cueCard{
-    constructor(question, answer, currentSide){
+    constructor(id){
         //current side is the side of the cue card we are viewing, 
         //either the side which has the question, or the side which has an answer
         //so its a boolean. 0 (false) for the question and 1 (true) for the answer
-        this.question=question;
-        this.answer=answer;
-        this.currentSide=currentSide;
+        this.id=id;
+        this.createCueCardElement();
     }
 
-    flipSide(){
-        if (this.currentSide=false){
-            this.currentSide=true;
-        }else{
-            this.currentSide=false;
-        }
+    createCueCardElement(){
+        var cueCardContainer=document.getElementById("cardDivs");
+        this.cueCardElement=document.createElement("div");
+        this.cueCardElement.id="cueCard"+this.id;
+        this.cueCardElement.style.display="none";
+    }
+
+    show() {
+        this.cueCardElement.style.display = "block"; 
+    }
+
+    hide() {
+        this.cueCardElement.style.display = "none";
     }
 };
+
+export default cueCard;
