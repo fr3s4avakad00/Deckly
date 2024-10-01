b0c2a694d762b6621e976ab829bf38ac05794e2a
PricklyProgrammer <96554231+PricklyProgrammer@users.noreply.github.com>
2024-10-01 16:19:37 -0400

    Restored files

---
+++ Deckly/Deckly/main.js
@@ -0,0 +1,16 @@
+function createQuiz() {
+    window.location.href = 'createQuiz.html';
+    
+  }
+  
+  function createCueCard() {
+    window.location.href = 'accountPage.html';
+  }
+  
+  function readNotes() {
+    window.location.href = 'readNote.html';
+  }
+
+  function exit() {
+    window.location.href = 'helo.html';
+  }
